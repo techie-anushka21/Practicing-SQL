0957de3f2d3b2f8befd487d4e0e436a0fa0e0d0f
Anushka Singh <ms.anushka2108@gmail.com>
2026-03-25 00:33:42 +0530

    Some Common Mathematical Functions In SQL

--- empTable.sql
+++ empTable.sql
@@ -1,12 +1,16 @@
---Creating our second table in SQL, will be moving to other concepts in upcoming programs.
---Also covered the following Common Mathematical Functions:-
---1. MAX() -> For maximum value.
---2. MIN() -> For minimum value.
---3. AVG() -> For average value.
---4. SUM() -> For sum (addition).
---5. COUNT() -> For totalling.
+--1. Creating our second table in SQL, will be moving to other concepts in upcoming programs.
+--2. Added Primary Key.
+--3. Inserted values in the table.
 
---Table creation
+--4. Also covered the following Common Mathematical Functions:-
+
+--i. MAX() -> For maximum value.
+--ii. MIN() -> For minimum value.
+--iii. AVG() -> For average value.
+--iv. SUM() -> For sum (addition).
+--v. COUNT() -> For totalling.
+
+--Creation of Table
 CREATE TABLE employees(
     emp_id NUMBER(5),
     emp_name CHAR(25) NOT NULL,
@@ -14,11 +18,11 @@ CREATE TABLE employees(
     salary NUMBER(15) NOT NULL
 );
 
---Adding "PRIMARY KEY"
+--Adding "PRIMARY KEY" to the Table
 ALTER TABLE employees
 ADD CONSTRAINT PK PRIMARY KEY(emp_id);
 
---Inserting values
+--Inserting values 
 INSERT INTO employees VALUES(1,'A','IT',60000);
 INSERT INTO employees VALUES(2,'B','HR',50000);
 INSERT INTO employees VALUES(3,'C','IT',75000);
@@ -26,19 +30,19 @@ INSERT INTO employees VALUES(4,'D','IT',45000);
 INSERT INTO employees VALUES(5,'E','HR',66000);
 INSERT INTO employees VALUES(6,'F','HR',42000);
 
-SELECT MAX(salary) FROM employees;    --returns "75000"
-SELECT MIN(salary) FROM employees;    --returns "42000"
-SELECT SUM(salary) FROM employees;    --returns "338000"
-SELECT AVG(salary) FROM employees;    --returns "56333.3333"
+SELECT MAX(salary) FROM employees;    --displays "75000"
+SELECT MIN(salary) FROM employees;    --displays "42000"
+SELECT SUM(salary) FROM employees;    --displays "338000"
+SELECT AVG(salary) FROM employees;    --displays "56333.3333"
 
 --Obtaining information from Specific Departments
-SELECT MAX(salary) FROM employees WHERE dept = 'HR';    --returns 66000
-SELECT MIN(salary) FROM employees WHERE dept = 'IT';    --returns 45000
+SELECT MAX(salary) FROM employees WHERE dept = 'HR';    --displays 66000
+SELECT MIN(salary) FROM employees WHERE dept = 'IT';    --displays 45000
 
-SELECT AVG(salary) FROM employees WHERE dept = 'HR';    --returns 52666.6666  
-SELECT AVG(salary) FROM employees WHERE dept = 'IT';    --returns 60000
+SELECT AVG(salary) FROM employees WHERE dept = 'HR';    --displays 52666.6666  
+SELECT AVG(salary) FROM employees WHERE dept = 'IT';    --displays 60000
 
-SELECT COUNT(*) FROM employees;    --returns 6 (total no. of employees in the company)
+SELECT COUNT(*) FROM employees;    --returns 6 (total no. of the employees in the company)
 
-SELECT COUNT(*) FROM employees WHERE dept = 'HR';    --return 3 (total no. of employees from "HR" Department)
-SELECT COUNT(*) FROM employees WHERE dept = 'IT';    --return 3 (total no. of employees from "IT" Department)
+SELECT COUNT(*) FROM employees WHERE dept = 'HR';    --return 3 (total no. of the employees from "HR" Department)
+SELECT COUNT(*) FROM employees WHERE dept = 'IT';    --return 3 (total no. of the employees from "IT" Department)
